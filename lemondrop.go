80db30a5bc6761525e96fb713849dbb0ca424063
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-22 19:44:35 -0700

    Increase context timeout

--- lemondrop.go
+++ lemondrop.go
@@ -45,7 +45,7 @@ func getCity(str string) (string, string, error) {
 }
 
 func fetchRegionsFromNetwork() (RegionDetails, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion("us-west-2"))
